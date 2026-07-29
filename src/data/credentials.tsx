68d1a563296992080e3ed2a534ddac249d71f35e
mhmdfirza <m.firza.aqila.r@gmail.com>
2026-07-29 09:48:26 +0700

    fix bug

--- src/data/credentials.tsx
+++ src/data/credentials.tsx
@@ -1,4 +1,5 @@
-export const WA_LINK = "https://wa.me/6281259791896?text=Halo%20kak,%20saya%20ingin%20melakukan%20booking.";
+export const WA_BASE = "6281259791896";
+export const WA_LINK = `https://wa.me/${WA_BASE}?text=Halo%20kak,%20saya%20ingin%20melakukan%20booking.`;
 export const HARI_BUKA = "Selasa- Minggu";
 export const JAM_BUKA = "08.00 - 18.00";
 export const ALAMAT = "Jl. Sidomukti No.12, Dsn. Bancaan, Ds. Mojosari, Kec. Kauman, Kab. Tulungagung, Jawa Timur 66261";
