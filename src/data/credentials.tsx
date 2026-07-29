913000d129677dd232d347e8688ea56fdb3d44cc
mhmdfirza <m.firza.aqila.r@gmail.com>
2026-07-29 09:45:48 +0700

    update credential

--- src/data/credentials.tsx
+++ src/data/credentials.tsx
@@ -1,1 +1,9 @@
 export const WA_LINK = "https://wa.me/6281259791896?text=Halo%20kak,%20saya%20ingin%20melakukan%20booking.";
+export const HARI_BUKA = "Selasa- Minggu";
+export const JAM_BUKA = "08.00 - 18.00";
+export const ALAMAT = "Jl. Sidomukti No.12, Dsn. Bancaan, Ds. Mojosari, Kec. Kauman, Kab. Tulungagung, Jawa Timur 66261";
+export const TELEPON = "081259791896";
+export const EMAIL = "risfatul@gmail.com";
+export const INSTAGRAM = "https://instagram.com/dealovasalon_id";
+export const FACEBOOK = "https://facebook.com/dealovasalon";
+export const TIKTOK = "https://www.tiktok.com/@dealovasalon";
